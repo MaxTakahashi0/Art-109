e92740da1e9cb398f0e439888b86b5c1382aab24
MaxTakahashi0 <100649503+MaxTakahashi0@users.noreply.github.com>
2026-04-13 17:10:42 -0700

    netart

---
+++ net-art-project/js/timing.js
@@ -0,0 +1,36 @@
+
+
+
+
+            $(document).ready(function () {
+
+        $('.falling').jGravity();
+    });
+
+// $('#activate').click(function () {
+//     // Target all divs with class 'box'
+//     $('div.box').jGravity({
+//         target: 'div', // 'everything' or 'this'
+//         ignoreClass: 'ignore', // class to ignore
+//         weight: 30, // gravity strength
+//         depth: 5, // z-index
+//         drag: true // make elements draggable
+//     });
+// });
+
+
+// Slider
+
+// async function refreshh() {
+//     btn.disabled = true;
+
+//     var result = await (await fetch(url)).text();
+
+//     document.getElementById("rangeInput").value = parseInt(result);
+//     document.getElementById("amount").value = result;
+
+//     setTimeout(refreshh, 3000);
+// }
+// document.addEventListener("DOMContentLoaded", function () {
+//     refreshh();
+// });
