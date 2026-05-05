88d8f1d1ba32e28a17f571d13a2f0f9aff6f5265
MaxTakahashi0 <100649503+MaxTakahashi0@users.noreply.github.com>
2026-05-04 22:17:39 -0700

    final

--- final-project/js/windows.js
+++ final-project/js/windows.js
@@ -1,23 +1,60 @@
+const sun = document.querySelector('.drainContainer');
+
+sun.addEventListener('click', () => {
+  sun.classList.toggle('is-paused');
+});
+
+
+sun.addEventListener("click", () => {
+
+    window.close();
+
+});
+
+
+
+function randomPopups() {
+    let count = Math.floor(Math.random() * 2) + 1;
+
+    for (let i = 0; i < count; i++) {
+        openWindow();
+    }
+}
 
 function openWindow() {
-    var i, l, options = [{
-       value: 'first',
-       text: 'First'
-    }, {
-       value: 'second',
-       text: 'Second'
-    }],
-    newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");  
+        let w = 200, h = 200;
 
-    newWindow.document.write("<select onchange='window.opener.setValue(this.value);'>");
-    for(i=0,l=options.length; i<l; i++) {
-        newWindow.document.write("<option value='"+options[i].value+"'>");  
-        newWindow.document.write(options[i].text);  
-        newWindow.document.write("</option>");
-    }
-    newWindow.document.write("</select>");
+    let left = Math.random() * (screen.availWidth - w);
+    let top = Math.random() * (screen.availHeight - h);
+
+    window.open("popups.html", "_blank",
+        `width=${w},height=${h},left=${left},top=${top}`);
 }
 
-function setValue(value) {
-    document.getElementById('value').value = value;
-}
+
+// function openWindow() {
+//     var i, l, options = [{
+//        value: 'first',
+//        text: 'First'
+//     }, {
+//        value: 'second',
+//        text: 'Second'
+//     }],
+//     newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");  
+
+//     newWindow.document.write("<select onchange='window.opener.setValue(this.value);'>");
+//     for(i=0,l=options.length; i<l; i++) {
+//         newWindow.document.write("<option value='"+options[i].value+"'>");  
+//         newWindow.document.write(options[i].text);  
+//         newWindow.document.write("</option>");
+//     }
+//     newWindow.document.write("</select>");
+
+// }
+
+// function setValue(value) {
+//     document.getElementById('value').value = value;
+// }
+
+
+
